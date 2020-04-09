9a42c2fc45fa98362d9c2710aa68b40279d89f24
eboshare <t.lila@list.ru>
2020-04-09 16:58:56 +0400

    Update example states

---
+++ states/demo/onboarding.py
@@ -0,0 +1,30 @@
+from aiogram.types import Message
+from django import forms
+
+from preparation import dispatcher as dp, content
+
+
+EMAIL_FIELD = forms.EmailField()
+
+
+@dp.state_handler(primary_state=True, bound=dp.message_handler)
+async def greeting(msg: Message):
+    await msg.answer(content.msg.greeting.render(first_name=msg.from_user.first_name))
+    return user_email
+
+
+@dp.state_handler(bound=dp.message_handler)
+async def user_email(msg: Message):
+    try:
+        EMAIL_FIELD.clean(msg.text)
+    except forms.ValidationError:
+        await msg.answer(content.msg.user_email.error)
+    else:
+        await msg.answer(content.msg.user_email.success)
+        # here must be logic to save the email somewhere :)
+        return dummy_state
+
+
+@dp.state_handler(bound=dp.message_handler)
+async def dummy_state(msg: Message):
+    pass
