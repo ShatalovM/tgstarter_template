8ab87bdc71d42bf228132cf83ffce3264ec97e68
eboshare <t.lila@list.ru>
2020-04-15 19:57:53 +0400

    Add the timezone parameter to trigger classes in scheduler

--- scheduler.py
+++ scheduler.py
@@ -7,7 +7,10 @@ from apscheduler.triggers.cron import CronTrigger
 from apscheduler.triggers.date import DateTrigger
 from apscheduler.triggers.interval import IntervalTrigger
 
-from preparation import scheduler
+from preparation import scheduler, config
+
+
+timezone = config.bot.timezone
 
 
 def main() -> None:
@@ -16,17 +19,17 @@ def main() -> None:
     loop.run_forever()
 
 
-@scheduler.scheduled_job(DateTrigger(run_date=datetime.now()))
+@scheduler.scheduled_job(DateTrigger(timezone=timezone, run_date=datetime.now()))
 async def date_job() -> None:
     print('USELESS JOB')
 
 
-@scheduler.scheduled_job(CronTrigger(day_of_week=calendar.MONDAY, hour=8, minute=0))
+@scheduler.scheduled_job(CronTrigger(timezone=timezone, day_of_week=calendar.MONDAY, hour=8, minute=0))
 async def cron_job() -> None:
     print('ULTIMATE NOTIFICATION')
 
 
-@scheduler.scheduled_job(IntervalTrigger(seconds=10))
+@scheduler.scheduled_job(IntervalTrigger(timezone=timezone, seconds=10))
 async def interval_job() -> None:
     print('10 SECONDS HAVE PASSED')
 
